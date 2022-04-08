947e60daeeb9dde3dba05698bc559dcfe11ff20c
katrin-fe <katrinfellner@gmx.net>
2022-04-08 10:15:18 +0200

    updated text index

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 $('.collapse').collapse()
+
+function changeColor(event) {
+    colorChangeAccordion.classList.remove("active");
+  
+}
+
+let colorChangeAccordion = document.querySelector("#accordion-button");
+colorChangeAccordion.addEventListener("click", changeColor);
